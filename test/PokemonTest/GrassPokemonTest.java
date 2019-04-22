d6402d395021eb82ff4b0bd15b44482e4bd68683
aleluman <aleluman@gmail.com>
2019-04-21 23:37:01 -0400

    Implemented different pokemon classes and its methods.

---
+++ test/PokemonTest/GrassPokemonTest.java
@@ -0,0 +1,9 @@
+package PokemonTest;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GrassPokemonTest {
+}
---
+++ test/PokemonTest/WaterPokemonTest.java
@@ -0,0 +1,10 @@
+package PokemonTest;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WaterPokemonTest {
+
+}
